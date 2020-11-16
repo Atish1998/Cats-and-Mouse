66fce32fa8cd6ff6f521641d64a706c0168dbf9f
Atish Mohapatra <69408821+Atish1998@users.noreply.github.com>
2020-11-16 09:21:46 +0530

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            int x = in.nextInt();
+            int y = in.nextInt();
+            int z = in.nextInt();
+            int diffzx= Math.abs(z-x);
+            int diffzy= Math.abs(z-y);
+            
+            if(diffzx>diffzy)
+                System.out.println("Cat B");
+            else if(diffzx<diffzy)
+                System.out.println("Cat A");
+                else
+                System.out.println("Mouse C");
+     }
+        }
+    }
+
